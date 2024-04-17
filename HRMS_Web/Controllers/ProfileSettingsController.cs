a4dafdc722751842ec002ac07a2b31fb2622cc9a
K Amith Malinga <malingaamith1@gmail.com>
2024-04-17 19:16:21 +0530

    Profile Settings | view details | backend

--- HRMS_Web/Controllers/ProfileSettingsController.cs
+++ HRMS_Web/Controllers/ProfileSettingsController.cs
@@ -1,15 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRMS_Web.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using HRMS_Web.Models;
-using Microsoft.AspNetCore.Authorization;
-using System.Diagnostics;
+using System.Security.Claims;
 
-namespace HRMS_Web.Controllers
+public class ProfileSettingsController : Controller
 {
-    public class ProfileSettingsController : Controller
+    private readonly ApplicationDbContext _context;
+
+    // Constructor to inject ApplicationDbContext
+    public ProfileSettingsController(ApplicationDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+
+
+    // Action to display all user details
+    public IActionResult Index(string id)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        // Now you have the userId, you can use it to retrieve user details from the database
+        var userDetails = _context.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+
+        if (userDetails == null)
+        {
+            return NotFound(); // Return 404 if user not found
+        }
+
+        return View(userDetails);
+    }
+
+
+    // Action to update user details
+    [HttpPost]
+    public IActionResult Update(ApplicationUser model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Update user details in the database
+            _context.ApplicationUser.Update(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index"); // Redirect to user details page
+        }
+
+        return View(model); // Return to the update form with validation errors
+    }
+
+
+
+    // Action to delete user details
+    public IActionResult Delete(String id)
+    {
+        // Retrieve user details from the database
+        var userDetails = _context.ApplicationUser.FirstOrDefault(u => u.Id == id);
+
+        if (userDetails == null)
+        {
+            return NotFound(); // Return 404 if user not found
+        }
+
+        _context.ApplicationUser.Remove(userDetails);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index"); // Redirect to user details page
+    }
+
+    
 }
