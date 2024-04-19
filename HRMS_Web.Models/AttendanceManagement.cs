3656c5dd02a7adf04f327c9ce27e17e6fc192ed3
weeramanthrie <weeramanthrietd@gmail.com>
2024-04-19 12:49:26 +0530

    Add fields to the Attendance table

---
+++ HRMS_Web.Models/AttendanceManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Web.Models
+{
+    public class AttendanceManagement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RecordId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public TimeSpan CheckIn { get; set; }
+        [Required]
+        public TimeSpan CheckOut { get; set; }
+        [Required]
+        public TimeSpan Break { get; set; }
+        [Required]
+        public TimeSpan OverTime { get; set; }
+    }
+}
