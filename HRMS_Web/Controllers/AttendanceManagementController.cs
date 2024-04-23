8dfb4834e6928394fab01c115320c0d94d073f37
K Amith Malinga <malingaamith1@gmail.com>
2024-04-23 12:32:24 +0530

    Revert "Add missing files to secondry attendance_managemnt branch"
    This reverts commit fcfa25d2b45fbae055fe0b20c05fb9012b9c6b70.

--- HRMS_Web/Controllers/AttendanceManagementController.cs
+++ HRMS_Web/Controllers/AttendanceManagementController.cs
@@ -1,16 +1,9 @@
-﻿using HRMS_Web.DataAccess.Data;
-using HRMS_Web.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HRMS_Web.Controllers
 {
     public class AttendanceManagementController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public AttendanceManagementController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
         public IActionResult Index()
         {
             return View();
@@ -21,12 +14,11 @@ namespace HRMS_Web.Controllers
         }
         public IActionResult MarkAttendance()
         {
-            return View();  
+            return View();
         }
         public IActionResult ViewHistory()
         {
-            List<AttendanceManagement> objAttendanceManagemetList = _db.AttendanceTimeTable.ToList();
-            return View(objAttendanceManagemetList);
+            return View();
         }
     }
 }
