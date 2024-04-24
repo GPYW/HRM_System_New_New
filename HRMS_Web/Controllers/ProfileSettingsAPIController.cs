1036a7ea4c58114de9531ce83d53f4ab1f7b4da6
GPYW <pasindiyathra@gmail.com>
2024-04-24 16:45:31 +0530

    API

---
+++ HRMS_Web/Controllers/ProfileSettingsAPIController.cs
@@ -0,0 +1,70 @@
+﻿using HRMS_Web.DataAccess.Data;
+using HRMS_Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileSettingsAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Constructor to inject ApplicationDbContext
+        public ProfileSettingsAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CategoryAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetApplicationUser(string id)
+        {
+            var user = await _context.ApplicationUser.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/CategoryAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationUser(string id, ApplicationUser user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.ApplicationUser.Any(e => e.Id == id);
+        }
+    }
+
+}
