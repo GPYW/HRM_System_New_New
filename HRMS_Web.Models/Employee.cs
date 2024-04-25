83b6a45507ed5147fc51b64888a1a234ff81cbf3
GPYW <pasindiyathra@gmail.com>
2024-04-25 17:36:58 +0530

    EmployeeManagementAPI

--- HRMS_Web.Models/Employee.cs
+++ HRMS_Web.Models/Employee.cs
@@ -5,13 +5,13 @@ namespace HRMS_Web.Models
     public class Employee
     {
         [Key]
-        public int EmployeeID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int MobileNo { get; set; }
+        public string? EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MobileNo { get; set; }
         [Required]
-        public string Email { get; set; }
-        public string Address { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
         public DateTime DOB { get; set; }
         public DateTime join_date { get; set; }
         
