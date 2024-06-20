85cab22b6578a4c79bfdbeadaa8969f798b7f551
Poojathmi Deepna <142476304+DEEPNAPOOja@users.noreply.github.com>
2024-06-20 13:04:09 +0530

    Projects Tasks Table connections

--- HRMS_Web.DataAccess/Data/ApplicationDbContext.cs
+++ HRMS_Web.DataAccess/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace HRMS_Web.DataAccess.Data
 {
@@ -16,6 +17,21 @@ namespace HRMS_Web.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
 
         public DbSet<AttendanceManagement> AttendanceTimeTable { get; set; }
+
+        //public DbSet<Projects> Projects { get; set; }
+
+        //public DbSet<Task> Tasks { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //    builder.Entity<Task>()
+        //        .HasOne(p => p.Project)
+        //        .WithMany(p => p.Tasks)
+        //        .HasForeignKey(p => p.ProjectID)
+               
+        //        ;
+        //}
 
 
     }
---
+++ HRMS_Web.Models/Tasks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Web.Models
+{
+    internal class Tasks
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+
+        public string? Task_Name { get; set; }
+
+        public string? Project_Name { get; set; }
+
+        public DateTime Due_Date { get; set; }
+
+        public string? Priority { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? Completion { get; set; }
+
+        public string? Actions { get; set; }
+
+        
+        public int ProjectID { get; set; }
+
+        public Projects Projects { get; set; }
+
+
+
+
+
+
+    }
+}
