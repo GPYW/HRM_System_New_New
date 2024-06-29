d87056eb973e05ef637f337271cea89f1bd63969
K Amith Malinga <malingaamith1@gmail.com>
2024-06-30 05:05:28 +0530

    Attendance bredcrum navigation

--- HRMS_Web/Controllers/AttendanceManagementController.cs
+++ HRMS_Web/Controllers/AttendanceManagementController.cs
@@ -16,24 +16,51 @@ namespace HRMS_Web.Controllers
             ViewData["Breadcrumb"] = new List<BreadcrumbItem>
         {
             new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
+            new BreadcrumbItem { Title = "Home", Url = Url.Action("Index", "AttendanceManagement") },
+
         };
             return View();
         }
         public IActionResult Reports()
         {
+            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
+                new BreadcrumbItem { Title = "Reports", Url = Url.Action("Reports", "AttendanceManagement") },
+            };
             return View();
         }
         public IActionResult MarkAttendance()
         {
+            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
+        {
+            new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
+            new BreadcrumbItem { Title = "Mark Attendance", Url = Url.Action("MarkAttendance", "AttendanceManagement") },
+
+        };
             return View();  
         }
         public IActionResult ViewHistory()
         {
             List<AttendanceManagement> objAttendanceManagemetList = _db.AttendanceTimeTable.ToList();
+            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
+        {
+            new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
+            new BreadcrumbItem { Title = "View History", Url = Url.Action("ViewHistory", "AttendanceManagement") },
+
+        };
             return View(objAttendanceManagemetList);
         }
         public IActionResult AddAttendance()
         {
+            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
+        {
+            new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
+            new BreadcrumbItem { Title = "Mark Attendance", Url = Url.Action("MarkAttendance", "AttendanceManagement") },
+            new BreadcrumbItem { Title = "Add Attendance", Url = Url.Action("AddAttendance", "AttendanceManagement") },
+
+
+        };
             return View();
         }
         [HttpPost]
