81ab3b83f3ba99e0317e1dca4b94d1f1a92ee9a8
K Amith Malinga <malingaamith1@gmail.com>
2024-07-04 21:44:51 +0530

    leaveeeeeeeeee

--- HRMS_Web.DataAccess/Migrations/20240704064913_leave.Designer.cs
+++ HRMS_Web.DataAccess/Migrations/20240704064913_leave.Designer.cs
@@ -4,6 +4,7 @@ using HRMS_Web.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HRMS_Web.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240704064913_leave")]
+    partial class leave
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
