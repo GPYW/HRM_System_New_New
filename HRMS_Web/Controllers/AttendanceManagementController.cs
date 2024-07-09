b65fc9473f29ad3d6a1ddad1a774efe4b9c48d5c
K Amith Malinga <malingaamith1@gmail.com>
2024-07-09 20:52:47 +0530

    Attendance report generating

--- HRMS_Web/Controllers/AttendanceManagementController.cs
+++ HRMS_Web/Controllers/AttendanceManagementController.cs
@@ -1,17 +1,22 @@
 ﻿using HRMS_Web.DataAccess.Data;
 using HRMS_Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Linq;
-using System.Collections.Generic;
-using System;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Globalization;
 
 namespace HRMS_Web.Controllers
 {
     public class AttendanceManagementController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public AttendanceManagementController(ApplicationDbContext db)
         {
             _db = db;
@@ -19,111 +24,45 @@ namespace HRMS_Web.Controllers
 
         public IActionResult Index()
         {
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "Home", Url = Url.Action("Index", "AttendanceManagement") },
-            };
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "Index");
             return View();
         }
 
         public IActionResult Reports()
         {
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "Reports", Url = Url.Action("Reports", "AttendanceManagement") },
-            };
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "Reports");
             return View();
         }
 
         public IActionResult MarkAttendance()
         {
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "Mark Attendance", Url = Url.Action("MarkAttendance", "AttendanceManagement") },
-            };
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "MarkAttendance");
             return View();
         }
 
         public IActionResult ViewHistory()
         {
-            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var applicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == loggedInUserId);
+            var objAttendanceManagemetList = GetAttendanceList();
 
-            if (applicationUser == null)
-            {
-                return NotFound("User not found");
-            }
-
-            var companyId = applicationUser.CompanyID;
-
-            List<AttendanceManagement> objAttendanceManagemetList = _db.AttendanceTimeTable
-                .Where(a => a.EmpID == companyId)
-                .OrderByDescending(a => a.Date)
-                .ToList();
-
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "View History", Url = Url.Action("ViewHistory", "AttendanceManagement") },
-            };
-
-            return View(objAttendanceManagemetList);
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "ViewHistory");
+            return View("ViewHistory", objAttendanceManagemetList);
         }
 
         [HttpPost]
         public IActionResult SearchAttendance(DateTime FromDate, DateTime ToDate)
         {
-            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var applicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == loggedInUserId);
+            var objAttendanceManagemetList = GetAttendanceList(FromDate, ToDate);
 
-            if (applicationUser == null)
-            {
-                return NotFound("User not found");
-            }
-
-            var companyId = applicationUser.CompanyID;
-
-            List<AttendanceManagement> objAttendanceManagemetList = _db.AttendanceTimeTable
-                .Where(a => a.EmpID == companyId && a.Date >= FromDate && a.Date <= ToDate)
-                .OrderByDescending(a => a.Date)
-                .ToList();
-
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "View History", Url = Url.Action("ViewHistory", "AttendanceManagement") },
-            };
-
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "ViewHistory");
             return View("ViewHistory", objAttendanceManagemetList);
         }
 
         [HttpPost]
         public IActionResult ShowAllAttendance()
         {
-            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var applicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == loggedInUserId);
+            var objAttendanceManagemetList = GetAttendanceList();
 
-            if (applicationUser == null)
-            {
-                return NotFound("User not found");
-            }
-
-            var companyId = applicationUser.CompanyID;
-
-            List<AttendanceManagement> objAttendanceManagemetList = _db.AttendanceTimeTable
-                .Where(a => a.EmpID == companyId)
-                .OrderByDescending(a => a.Date)
-                .ToList();
-
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "View History", Url = Url.Action("ViewHistory", "AttendanceManagement") },
-            };
-
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "ViewHistory");
             return View("ViewHistory", objAttendanceManagemetList);
         }
 
@@ -177,12 +116,7 @@ namespace HRMS_Web.Controllers
 
         public IActionResult AddAttendance()
         {
-            ViewData["Breadcrumb"] = new List<BreadcrumbItem>
-            {
-                new BreadcrumbItem { Title = "Attendance Management", Url = Url.Action("Index", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "Mark Attendance", Url = Url.Action("MarkAttendance", "AttendanceManagement") },
-                new BreadcrumbItem { Title = "Add Attendance", Url = Url.Action("AddAttendance", "AttendanceManagement") },
-            };
+            ViewData["Breadcrumb"] = GetDefaultBreadcrumb("Attendance Management", "MarkAttendance", "AddAttendance");
             return View();
         }
 
@@ -192,6 +126,180 @@ namespace HRMS_Web.Controllers
             _db.AttendanceTimeTable.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("MarkAttendance");
+        }
+
+        [HttpPost]
+        public IActionResult PDF(int month, int year)
+        {
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var applicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == loggedInUserId);
+
+            if (applicationUser == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var employeeId = applicationUser.CompanyID;
+            var employeeName = applicationUser.FirstName + " " + applicationUser.LastName;
+
+            List<AttendanceManagement> attendanceList = _db.AttendanceTimeTable
+                .Where(a => a.EmpID == employeeId && a.Date.Month == month && a.Date.Year == year)
+                .OrderByDescending(a => a.Date)
+                .ToList();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (iTextSharp.text.Document document = new iTextSharp.text.Document(PageSize.A4, 25, 25, 30, 30))
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(document, ms);
+                    document.Open();
+
+                    var image = iTextSharp.text.Image.GetInstance("wwwroot/images/LetterHead.png");
+                    float pageWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+                    image.ScaleToFit(pageWidth, image.Height * (pageWidth / image.Width));
+                    image.Alignment = Element.ALIGN_CENTER;
+                    document.Add(image);
+
+                    var footer = FontFactory.GetFont("Arial", 8, Font.NORMAL);
+                    var subTitleFont = FontFactory.GetFont("Arial", 10, Font.BOLD);
+                    var regularFont = FontFactory.GetFont("Arial", 10, Font.NORMAL);
+
+                    document.Add(new Paragraph($"Attendance Report - {year} {CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month)}", subTitleFont));
+                    document.Add(new Paragraph(" "));
+                    document.Add(new Paragraph($"Employee ID: {employeeId}", regularFont));
+                    document.Add(new Paragraph($"Employee Name: {employeeName}", regularFont));
+                    document.Add(new Paragraph($"Month : {CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month)}", regularFont));
+                    document.Add(new Paragraph(" "));
+
+                    PdfPTable table = new PdfPTable(6);
+                    table.WidthPercentage = 100;
+                    table.DefaultCell.Border = PdfPCell.NO_BORDER;
+
+                    void AddHeaderCell(PdfPTable t, string text)
+                    {
+                        var font = FontFactory.GetFont("Arial", 10, Font.BOLD, BaseColor.WHITE);
+                        PdfPCell cell = new PdfPCell(new Phrase(text, font))
+                        {
+                            HorizontalAlignment = Element.ALIGN_CENTER,
+                            VerticalAlignment = Element.ALIGN_MIDDLE,
+                            BackgroundColor = new BaseColor(25, 161, 184),
+                            Border = PdfPCell.NO_BORDER
+                        };
+                        t.AddCell(cell);
+                    }
+
+                    void AddBodyCell(PdfPTable t, string text, bool isOdd)
+                    {
+                        var font = FontFactory.GetFont("Arial", 8, Font.NORMAL);
+                        PdfPCell cell = new PdfPCell(new Phrase(text, font))
+                        {
+                            HorizontalAlignment = Element.ALIGN_CENTER,
+                            VerticalAlignment = Element.ALIGN_MIDDLE,
+                            Border = PdfPCell.NO_BORDER
+                        };
+                        if (isOdd)
+                        {
+                            cell.BackgroundColor = BaseColor.WHITE;
+                        }
+                        else
+                        {
+                            cell.BackgroundColor = new BaseColor(185, 212, 217);
+                        }
+                        t.AddCell(cell);
+                    }
+
+                    AddHeaderCell(table, "No");
+                    AddHeaderCell(table, "Date");
+                    AddHeaderCell(table, "Check-In");
+                    AddHeaderCell(table, "Check-Out");
+                    AddHeaderCell(table, "Overtime");
+                    AddHeaderCell(table, "Break");
+
+                    bool isOddRow = true;
+                    int counter = 1;
+                    foreach (var attendance in attendanceList)
+                    {
+                        AddBodyCell(table, counter.ToString(), isOddRow);
+                        AddBodyCell(table, attendance.Date.ToString("yyyy-MM-dd"), isOddRow);
+                        AddBodyCell(table, attendance.CheckIn.ToString(@"hh\:mm"), isOddRow);
+                        AddBodyCell(table, attendance.CheckOut.ToString(@"hh\:mm"), isOddRow);
+                        AddBodyCell(table, attendance.OverTime, isOddRow);
+                        AddBodyCell(table, attendance.Break, isOddRow);
+
+                        isOddRow = !isOddRow;
+                        counter++;
+                    }
+
+                    document.Add(table);
+                    document.Add(new Paragraph(" "));
+
+                    document.Add(new Paragraph("----End of the document----", footer));
+
+                    document.Add(new Paragraph("*This document is system-generated and does not require a signature.", footer));
+                    document.Add(new Paragraph(" "));
+
+                    document.Close();
+
+                    byte[] byteArray = ms.ToArray();
+                    return File(byteArray, "application/pdf", $"Report_{year}_{month}.pdf");
+                }
+            }
+        }
+
+
+        // Helper method to fetch attendance list based on filters
+        private List<AttendanceManagement> GetAttendanceList(DateTime? fromDate = null, DateTime? toDate = null)
+        {
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var applicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == loggedInUserId);
+
+            if (applicationUser == null)
+            {
+                return new List<AttendanceManagement>(); // Or handle appropriately
+            }
+
+            var companyId = applicationUser.CompanyID;
+
+            var query = _db.AttendanceTimeTable.Where(a => a.EmpID == companyId);
+
+            if (fromDate.HasValue && toDate.HasValue)
+            {
+                query = query.Where(a => a.Date >= fromDate && a.Date <= toDate);
+            }
+
+            var objAttendanceManagemetList = query.OrderByDescending(a => a.Date).ToList();
+
+            return objAttendanceManagemetList;
+        }
+
+        // Helper method to get default breadcrumb items
+        private List<BreadcrumbItem> GetDefaultBreadcrumb(string mainTitle, string currentAction, string currentController = "AttendanceManagement")
+        {
+            return new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Title = mainTitle, Url = Url.Action("Index", currentController) },
+                new BreadcrumbItem { Title = GetBreadcrumbTitle(currentAction), Url = Url.Action(currentAction, currentController) },
+            };
+        }
+
+        // Helper method to get breadcrumb title based on action
+        private string GetBreadcrumbTitle(string action)
+        {
+            switch (action)
+            {
+                case "Index":
+                    return "Home";
+                case "Reports":
+                    return "Reports";
+                case "MarkAttendance":
+                    return "Mark Attendance";
+                case "ViewHistory":
+                    return "View History";
+                case "AddAttendance":
+                    return "Add Attendance";
+                default:
+                    return action;
+            }
         }
     }
 }
