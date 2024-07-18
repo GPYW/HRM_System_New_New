c78bc57ee8a435025aeede9820ae6e5788d3a1ec
weeramanthrie <weeramanthrietd@gmail.com>
2024-07-18 22:15:24 +0530

    Performance

---
+++ HRMS_Web/Controllers/PerformanceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS_Web.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        private readonly IPerformanceService _performanceService;
+
+        public PerformanceController(IPerformanceService performanceService)
+        {
+            _performanceService = performanceService;
+        }
+
+        public IActionResult Dashboard()
+        {
+            var model = _performanceService.GetDashboardData(User.Identity.Name);
+            return View(model);
+        }
+    }
+}
